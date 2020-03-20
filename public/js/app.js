e84bb29772b564754595a920fb9e9e0c078e2d6b
Kathia918 <kathiamarce17@gmail.com>
2020-03-19 21:27:43 -0600

    Ultimos

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,6 @@ document.addEventListener("DOMContentLoaded", event => {
     
     });
 }); 
-
 //PETICION Y COLOCA LA VISTA SEGUN EL MODULO QUE SE LE HA SIDO ENVIADO
  function colocarVista(modulo){
 
