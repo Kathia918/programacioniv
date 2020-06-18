8c132e4444006b935f137a5929bd51c1700c5ec6
Kathia918 <kathiamarce17@gmail.com>
2020-06-18 15:25:21 -0600

    Pagina2

--- mineria.py
+++ mineria.py
@@ -2,10 +2,10 @@
 from facebook_scraper import get_posts
 import csv
 
-face = csv.writer(open('Dsugar.csv','w'))
+face = csv.writer(open('Biblioteca.csv','w'))
 face.writerow(['POST_ID','TEXTO','LIKES'])
 
-for post in get_posts('Dsugarclothes ', pages=10, ):
+for post in get_posts('Bibliotecaugb ', pages=10, ):
     #print(post.keys())
     print(post['post_id'], post['text'], post['likes'])
     try:
