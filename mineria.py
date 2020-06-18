40058225ed93b0b171ba2847985b2c88e773f642
Kathia918 <kathiamarce17@gmail.com>
2020-06-17 22:08:50 -0600

    KMBA

--- mineria.py
+++ mineria.py
@@ -1,1 +1,14 @@
 #parcial_III
+from facebook_scraper import get_posts
+import csv
+
+face = csv.writer(open('ugb.csv','w'))
+face.writerow(['POST_ID','TEXTO','LIKES'])
+
+for post in get_posts('ugbstoreoficial', pages=5, ):
+    #print(post.keys())
+    print(post['post_id'], post['text'], post['likes'])
+    try:
+        face.writerow([post['post_id'], post['text'], post['likes']])
+    except:
+        None
