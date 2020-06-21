91dfc98ea5ebba35d4b1367290ba3eb836127810
Kathia918 <kathiamarce17@gmail.com>
2020-06-20 22:56:04 -0600

    Cambios

--- Biblioteca.py
+++ Biblioteca.py
@@ -2,13 +2,13 @@
 from facebook_scraper import get_posts
 import csv
 
-face = csv.writer(open('Biblioteca.csv','w'))
+face = csv.writer(open('biblioteca.csv','w'))
 face.writerow(['POST_ID','TEXTO','LIKES','IMAGE'])
 
 for post in get_posts('bibliotecaugb ', pages=5, ):
     #print(post.keys())
-    print(post['post_id'], post['text'], post['likes'],post['image'], sep="-")
+    print(post['post_id'], post['text'], post['likes'], post['image'], sep="-")
     try:
-        face.writerow([post['post_id'], post['text'], post['likes'],post['image']])
+        face.writerow([post['post_id'], post['text'], post['likes'], post['image']])
     except:
         None
