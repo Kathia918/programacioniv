d827a54ee63c66de2603d6ada340fab551816ad4
Kathia918 <kathiamarce17@gmail.com>
2020-06-20 23:15:37 -0600

    Cambios

--- Maxi.py
+++ Maxi.py
@@ -2,10 +2,10 @@
 from facebook_scraper import get_posts
 import csv
 
-face = csv.writer(open('biblioteca.csv','w'))
+face = csv.writer(open('Maxi.csv','w'))
 face.writerow(['POST_ID','TEXTO','LIKES','IMAGE'])
 
-for post in get_posts('bibliotecaugb ', pages=7, ):
+for post in get_posts('ferremaxi ', pages=7, ):
     #print(post.keys())
     print(post['post_id'], post['text'], post['likes'], post['image'], sep="-")
     try:
