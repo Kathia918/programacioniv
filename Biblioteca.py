07d2b63709bfb60deb8bc6b84941083b9e282fe3
Kathia918 <kathiamarce17@gmail.com>
2020-06-20 22:04:14 -0600

    Cam

--- Biblioteca.py
+++ Biblioteca.py
@@ -7,7 +7,7 @@ face.writerow(['POST_ID','TEXTO','LIKES','IMAGE'])
 
 for post in get_posts('bibliotecaugb ', pages=5, ):
     #print(post.keys())
-    print(post['post_id'], post['text'], post['likes'], post['image'], sep="-")
+    print(post['post_id'], post['text'], post['likes'], post['image'])
     try:
         face.writerow([post['post_id'], post['text'], post['likes'], post['image']])
     except:
