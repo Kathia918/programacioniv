7a8d8f1ccf0aeb75743c6f12aae7a80fb88eddf1
Kathia918 <kathiamarce17@gmail.com>
2020-06-20 21:21:31 -0600

    Cambios_KathiaBerrios

---
+++ Biblioteca.py
@@ -0,0 +1,14 @@
+#parcial_III
+from facebook_scraper import get_posts
+import csv
+
+face = csv.writer(open('Biblioteca.csv','w'))
+face.writerow(['POST_ID','TEXTO','LIKES','IMAGE'])
+
+for post in get_posts('bibliotecaugb ', pages=5, ):
+    #print(post.keys())
+    print(post['post_id'], post['text'], post['likes'], post['image'], sep="-")
+    try:
+        face.writerow([post['post_id'], post['text'], post['likes'], post['image']])
+    except:
+        None
